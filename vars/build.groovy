861b131a3544c3e29afcaad516f780b4e588f172
Amir <dahmouni_amir@hotmail.fr>
2023-10-22 20:44:24 +0100

    use params

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,14 +1,23 @@
 #!/usr/bin/env/ groovy
-def BuildImage() {
-    echo "building the application ..."
-    sh "docker-buildx build -t 12851043/weather_api_app:1.0 ."
-    sh "docker-buildx build -t localhost:8082/weather_api_app:1.0 ."
-    echo "Login to dockerHub ..."
-    sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
-    echo "Login to Nexus ..."
-    sh "echo $PASSWORD_NEXUS | docker login -u $USER_NEXUS --password-stdin localhost:8082"
-    echo "Login to Dockerhub ..."
-    sh "docker push 12851043/weather_api_app:1.0"
-    echo "Login to Nexus ..."
-    sh "docker push localhost:8082/weather_api_app:1.0"
+
+def BuildImage(String dockerImageName, String nexusImageName ) {
+    withCredentials([
+            usernamePassword(credentialsId: 'Dockerhub', usernameVariable: 'USER_DOCKER', passwordVariable: 'PASSWORD_DOCKER'),
+            usernamePassword(credentialsId: 'Nexus', usernameVariable: 'USER_NEXUS', passwordVariable: 'PASSWORD_NEXUS')
+    ]) {
+        echo "variable ${ENV}"
+        echo "deploying application version ${params.VERSION}... "
+        echo "building the application ..."
+        sh "docker-buildx build -t $dockerImageName ."
+        sh "docker-buildx build -t $nexusImageName ."
+        echo "Login to dockerHub ..."
+        sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
+        echo "Login to Nexus ..."
+        sh "echo $PASSWORD_NEXUS | docker login -u $USER_NEXUS --password-stdin localhost:8082"
+        echo "Push image to Dockerhub ..."
+        sh "docker push $dockerImageName"
+        echo "Push image to Nexus ..."
+        sh "docker push $nexusImageName"
+    }
+
 }
