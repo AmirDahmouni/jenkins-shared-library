ce2d546494cb1a09411fa747797746c334fe62bf
Amir <dahmouni_amir@hotmail.fr>
2023-10-25 12:32:41 +0100

    refactor build steps

---
+++ vars/buildDocker.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env/ groovy
+
+def call( String dockerImageName ) {
+    withCredentials([
+            usernamePassword(credentialsId: 'Dockerhub', usernameVariable: 'USER_DOCKER', passwordVariable: 'PASSWORD_DOCKER')
+    ]) {
+        echo "Environement: ${ENV}"
+        echo "deploying application version ${params.VERSION}... "
+        echo "building the Docker image ..."
+        sh "docker-buildx buildDocker -t $dockerImageName ."
+        echo "Login to dockerHub ..."
+        sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
+        echo "Push image to Dockerhub ..."
+        sh "docker push $dockerImageName"
+    }
+
+}
---
+++ vars/buildNexus.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env/ groovy
+
+def call( String nexusImageName ) {
+    withCredentials([
+            usernamePassword(credentialsId: 'Nexus', usernameVariable: 'USER_NEXUS', passwordVariable: 'PASSWORD_NEXUS')
+    ]) {
+        echo "Environement: ${ENV}"
+        echo "deploying application version ${params.VERSION}... "
+        echo "building the Nexus Image ..."
+        sh "docker-buildx buildDocker -t $nexusImageName ."
+        echo "Login to Nexus ..."
+        sh "echo $PASSWORD_NEXUS | docker login -u $USER_NEXUS --password-stdin localhost:8082"
+        echo "Push image to Nexus ..."
+        sh "docker push $nexusImageName"
+    }
+
+}
