f1760bcbbdd627df0ef837422e3236460aaae597
Amir <dahmouni_amir@hotmail.fr>
2023-10-25 23:24:53 +0100

    remove $

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -7,7 +7,7 @@ def call( String dockerImageName ) {
         echo "Environement: ${ENV}"
         echo "deploying application version ${params.VERSION}... "
         echo "building the Docker image ..."
-        sh 'docker-buildx build -t'+ $dockerImageName+ '. '
+        sh 'docker-buildx build -t'+ dockerImageName+ '. '
         echo "Login to dockerHub ..."
         sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
         echo "Push image to Dockerhub ..."
