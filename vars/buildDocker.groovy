12769f412cbeec3cd40d8529a5c188b5587b0066
Amir <dahmouni_amir@hotmail.fr>
2023-10-26 12:17:18 +0100

    + \\

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -4,13 +4,11 @@ def call( String dockerImageName ) {
     withCredentials([
             usernamePassword(credentialsId: 'Dockerhub', usernameVariable: 'USER_DOCKER', passwordVariable: 'PASSWORD_DOCKER')
     ]) {
-        def directory ="./"
-        def build="docker-buildx build -t $dockerImageName $directory"
         echo "Environement: ${ENV}"
         echo "deploying application version ${params.VERSION}... "
         echo "building the Docker image ..."
         echo build
-        sh build
+        sh "docker-buildx build -t $dockerImageName \\."
         echo "Login to dockerHub ..."
         sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
         echo "Push image to Dockerhub ..."
