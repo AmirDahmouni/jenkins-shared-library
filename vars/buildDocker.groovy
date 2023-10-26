1bb7063abb1dbbc453d01110a380728b4e19d702
Amir <dahmouni_amir@hotmail.fr>
2023-10-26 12:54:15 +0100

    fix v3

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -7,7 +7,7 @@ def call( String dockerImageName ) {
         echo "Environement: ${ENV}"
         echo "deploying application version ${params.VERSION}... "
         echo "building the Docker image ..."
-        sh 'Build="docker-buildx build -t ${dockerImageName} ./"'
+        sh " Build='docker-buildx build -t ${dockerImageName} ./' "
         sh "echo $Build"
         sh "$Build"
         echo "Login to dockerHub ..."
