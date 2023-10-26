d20e192f56b14a4d384241d42878e4e99d88d741
Amir <dahmouni_amir@hotmail.fr>
2023-10-26 12:52:04 +0100

    fix v2

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -8,7 +8,7 @@ def call( String dockerImageName ) {
         echo "deploying application version ${params.VERSION}... "
         echo "building the Docker image ..."
         sh 'Build="docker-buildx build -t ${dockerImageName} ./"'
-        sh 'echo $Build'
+        sh "echo $Build"
         sh "$Build"
         echo "Login to dockerHub ..."
         sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
