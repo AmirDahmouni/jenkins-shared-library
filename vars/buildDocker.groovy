92002072e8af87923e29d884afdfa581480ec24d
Amir <dahmouni_amir@hotmail.fr>
2023-10-26 12:47:37 +0100

    roll back

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -7,7 +7,9 @@ def call( String dockerImageName ) {
         echo "Environement: ${ENV}"
         echo "deploying application version ${params.VERSION}... "
         echo "building the Docker image ..."
-        sh "docker-buildx build -t ${dockerImageName} ./"
+        sh 'Build="docker-buildx build -t ${dockerImageName}" ./'
+        sh 'echo $Build'
+        sh "Build"
         echo "Login to dockerHub ..."
         sh "echo $PASSWORD_DOCKER | docker login -u $USER_DOCKER --password-stdin"
         echo "Push image to Dockerhub ..."
